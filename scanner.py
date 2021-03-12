68c53be7ca2e7b48cd8f643ae89e8123ae70d790
Aditya Manwar <71894763+Aditya021-git@users.noreply.github.com>
2021-03-12 14:49:14 +0530

    Update and rename adh.py to scanner.py

--- scanner.py
+++ scanner.py
@@ -12,38 +12,16 @@ from pyzbar.pyzbar import ZBarSymbol
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-"""
-x_data = np.array( [np.array(cv2.imread('adh.jpg'))] )
-
-pixels = x_data.flatten()
-print (pixels.shape)
-
-
-img = cv2.imread('adh.jpg')
-rows,cols,ch = img.shape
-
-pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
-
-M = cv2.getPerspectiveTransform(pts1,pts2)
-
-dst = cv2.warpPerspective(img,M,(300,300))
-
-plt.subplot(121),plt.imshow(img),plt.title('Input')
-plt.subplot(122),plt.imshow(dst),plt.title('Output')
-plt.show()
-
-"""
-
 img = cv2.imread('sample1.png')
-img2 =cv2.imread('Sample.png')
-"""
+
+#img2 =cv2.imread('Sample.png')
+
+
 cv2.imshow("img",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-
+"""
 cv2.imshow("img2",img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -54,7 +32,7 @@ data, bbox, qr = detector.detectAndDecode(img)
 """
 
 gray1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+#gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 
 qrcode = pyzbar.decode(img)
@@ -65,7 +43,7 @@ for qrcod in qrcode:
     qtype = qrcod.type
     text = f"{qdata},{qtype}"
     cv2.putText(img,text,(x,y-10),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,0.5,(0,255,0),1)
-
+"""
 qrcode2 = pyzbar.decode(img2)
 for qrcod in qrcode2:
     x,y,w,h = qrcod.rect
@@ -74,7 +52,7 @@ for qrcod in qrcode2:
     qtype = qrcod.type
     text = f"{qdata},{qtype}"
     cv2.putText(img2,text,(x,y-10),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,0.5,(0,255,0),1)
-    
+"""    
 cv2.imshow("img",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
